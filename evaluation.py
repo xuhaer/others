f28b61cf5e42d1d38db9342dce2299276eca9668
hardly <trigal@qq.com>
2018-12-20 18:16:00 +0800

    structure changed

--- evaluation.py
+++ evaluation.py
@@ -7,6 +7,7 @@ import jieba
 import numpy as np
 import pandas as pd
 
+# jieba.load_userdict('st_dict.txt')
 
 def dtype_cnt(x):
     """判断某列中某cell的数据类型"""
@@ -18,90 +19,72 @@ def dtype_cnt(x):
         return 'numeric'
 
 
-def col_type_cnt(data):
+def col_type_cnt(df, result):
     """
     统计原始数据各列数据类型
-        params: data--pd.df
+        params: df--pd.df
         return: 以列名为key, 'numeric'、'str'、'bool'、'nan'
                 的个数值构成的列表的字典
     """
     res, z = defaultdict(list), {}
-    result = defaultdict(dict)
-    for col in data:
-        cnt_dict = dict(Counter(data[col].map(dtype_cnt)))
+    for col in df:
+        cnt_dict = dict(Counter(df[col].map(dtype_cnt)))
         result[col].update({x: cnt_dict.setdefault(x, np.nan) for x in ['numeric', 'str', 'nan']})
     return result
 
 
-def gen_st_names(s_t):
+def predict_st_names(df, result):
     """
     构建以nameChs 和 nameChsAlts 为键，name的set 为值的字典
         如:{'空腹血糖': {'GLU'}, '血糖': {'GLU'}, '白细胞': {'ULEU', 'WBC', 'stool07'} ......}
     """
-    st_names = defaultdict(set)
-    for row in s_t.loc[ :, ['name', 'nameChs', 'nameChsAlts']].fillna('').values:
-        st_names[row[1]].add(f'{row[0]}')
-        if  ',' in row[2]:
-            for nameChsAlts in row[2].split(','):
-                st_names[nameChsAlts].add(f'{row[0]}')
-        elif row[2] and row[2] != ' ':
-            st_names[row[2]].add(f'{row[0]}')
-    return st_names
+    with open('/Users/har/Desktop/st_names_predict.json') as f:
+        predict_map = json.load(f)
+
+    for col in df.columns:
+        likely_name = []
+        # 如果col为: `一般检查-身高`, 不直接把 `一般检查-身高` 拿去分词, 后续col取 `身高`
+        sub_col = col.split('-')[-1]
+        if predict_map.get(sub_col):
+            result[col].update({'匹配列名': predict_map[sub_col].split(',')})
+        else:
+            for x in jieba.cut(sub_col):
+                if x in predict_map:
+                    likely_name.extend(predict_map[x].split(','))
+            if likely_name:
+                result[col].update({'相似列名': likely_name})
 
 
-def col_values_check(data, st_names, s_t, result):
-    """检查data中各列数据的合法性
+def col_values_check(df, s_t, result):
+    """检查df中各列对应人群的健康情况
         先决条件:
-                1.data中的带检查列(col)数据类型为['int64', 'float64']
-                2.data中的带检查列(col)与prc中的st有关联--存在或相似
+                1.df中的带检查列(col)数据类型为['int64', 'float64']
+                2.df中的带检查列(col)与prc中的st有关联--存在或相似
     """
-    legal_cols = set()
-    for col in data.columns:
-        likely_name = set()
-        if st_names.get(col):
-            result[col].update({'对应prc列名': ','.join(st_names[col])})
-        for x in jieba.cut(col):
-            if x in st_names:
-                likely_name.update(st_names[x])
-        if likely_name:
-            result[col].update({'可能列名': ','.join(likely_name)})
-        # 数据检测
-        if '可能列名' in result[col] or '对应prc列名' in result[col]:
-            flag = '可能列名' if '可能列名' in result[col] else '对应prc列名'
-            for like_name in result[col][flag].split(','):
-                the_row = s_t[s_t['name'] == like_name].loc[:, ['nameChs', 'nameChsAlts', 'dataType', 'defaultRefRange', 'unit']].fillna('').to_dict(orient='records')
-                # the row 肯定不为空
-                the_row = the_row[0]
-                the_range = json.loads(the_row['defaultRefRange'])
-                the_type = the_row['dataType']
-                if data[col].dtypes in ['int64', 'float64'] \
-                    and the_range and the_type in ['int', 'float', '[float]']:
-                    if isinstance(the_range, dict):
-                        legal_range = {}
-                        for v in the_range.values():
-                            if isinstance(v, dict):
-                                for v in v.values():
-                                    if legal_range.setdefault('min', v[0]) > v[0]:
-                                        legal_range['min'] = v[0]
-                                    if legal_range.setdefault('max', v[1]) < v[1]:
-                                        legal_range['max'] = v[1]
-                            else:
-                                if legal_range.setdefault('min', v[0]) > v[0]:
-                                    legal_range['min'] = v[0]
-                                if legal_range.setdefault('max', v[1]) < v[1]:
-                                    legal_range['max'] = v[1]
-                    elif isinstance(the_range, list):
-                        legal_range = {'min': the_range[0], 'max': the_range[1]}
-                    # legal_range为prc中defaultRange允许的最大范围
-                    if data[col].min() < float(legal_range['min']) or \
-                        data[col].max() > float(legal_range['max']):
-                        sorted_col = np.sort(data[col].dropna())
-                        illegal_min = bisect_left(sorted_col, legal_range['min'])
-                        illegal_max = len(sorted_col) - bisect_right(sorted_col, legal_range['max'])
-                        (illegal_min + illegal_max) / len(data[col])
-                        result[col].update({'非法数据': f'约{(illegal_min + illegal_max) / len(data[col])*100:.2f}%', '合法范围': [legal_range['min'], legal_range['max']]})
-                    else:
-                        result[col].update({'非法数据': '0%', '合法范围': [legal_range['min'], legal_range['max']]})
+    for col in df.columns:
+        if '相似列名' in result[col] or '匹配列名' in result[col]:
+            flag = '相似列名' if '相似列名' in result[col] else '匹配列名'
+            # 多个匹配项取第一个来预测评估
+            matched_name = result[col][flag][0]
+            if not matched_name in s_t.index:
+                continue
+            # 能到这一步，说明该col类型在prc中为float, 可强制转换
+            legal_range = json.loads(s_t.loc[matched_name, :]['RefRange'])
+            try:
+                df[col] = pd.to_numeric(df[col], errors='coerce')
+            except ValueError:
+                # This should not happen.
+                pass
+            if legal_range['max'] == 'Infinity':
+                legal_range['max'] = 9999
+            if df[col].min() < float(legal_range['min']) or \
+                df[col].max() > float(legal_range['max']):
+                sorted_col = np.sort(df[col].dropna())
+                health_min = bisect_left(sorted_col, legal_range['min'])
+                health_max = len(sorted_col) - bisect_right(sorted_col, legal_range['max'])
+                result[col].update({'指标异常比例': f'约{(health_min + health_max) / len(df[col])*100:.2f}%'})
+            else:
+                result[col].update({'指标异常比例': '0%'})
 
 
 def highlight_max_data(data, attr):
@@ -134,18 +117,33 @@ def highlight_illegal_max_min(data, attr):
     return [attr if v else '' for v in illegal]
 
 
-def evaluation(data, s_t, output_path='/Users/har/Desktop/安徽电信2018数据/evaluation.xls'):
-    st_names = gen_st_names(s_t)
-    evaluation = col_type_cnt(data)
-    col_values_check(data, st_names, s_t, evaluation)
-    df1 = pd.DataFrame(evaluation)
-    df = pd.concat([df1, data.describe(include='all').loc[['unique', 'top', 'freq', 'min', 'max']]], sort=False)
-    style_df = df.T.style.\
-        applymap(highlight_illegal_data, subset=['非法数据']).\
-        apply(highlight_illegal_max_min, attr='color: red', axis=1, subset=['合法范围', '非法数据', 'min', 'max'])
+def evaluation(df, s_t, output_path):
+    result = defaultdict(dict)
+    col_type_cnt(df, result)
+    predict_st_names(df, result)
+    col_values_check(df, s_t, result)
 
-    style_df.to_excel(output_path, engine='openpyxl', float_format='%.3f')
+    df1 = pd.DataFrame(result)
+    df = pd.concat([df1, df.describe(include='all').loc[['unique', 'top', 'freq', 'min', 'max']]], sort=False)
+    # style_df = df.T.style.\
+    #     applymap(highlight_illegal_data, subset=['非法数据']).\
+    #     apply(highlight_illegal_max_min, attr='color: red', axis=1, subset=['合法范围', '非法数据', 'min', 'max'])
+    # 染色美化跑杨子石化有点问题,可暂不染色
+    # style_df.to_excel(output_path, engine='openpyxl', float_format='%.3f')
+    df.T.to_excel(output_path, float_format='%.3f')
 
-merged = pd.read_excel('/Users/har/Desktop/安徽电信2018数据/merged.xls')
-s_t = pd.read_csv('/Users/har/Desktop/安徽电信2018数据/T_SampleType_201811121655.csv')
-evaluation(merged, s_t, '/Users/har/Desktop/安徽电信2018数据/evaluation_1.xls')
+
+
+# input_df = pd.read_excel('/Users/har/Desktop/许某/2018贮运厂.xls', header=[0, 1])
+# # 支持2级表头
+# input_df.columns = input_df.columns.map(lambda x: '-'.join(x) if 'Unnamed' not in x[1] else x[0])
+# input_df.reset_index(drop=True, inplace=True)
+# input_df.drop_duplicates(inplace=True)
+# input_df.to_excel('/Users/har/Desktop/许某/2018贮运厂表头合并后.xls')
+input_df = pd.read_excel('/Users/har/Desktop/许某/2018质检中心_表头合并后.xls')
+s_t = pd.read_excel('/Users/har/Desktop/T_SampleType.xls')
+s_t.set_index('name', inplace=True)
+s_t.dropna(how='all')
+s_t.fillna('', inplace=True)
+s_t = s_t[s_t['RefRange'] != '']
+evaluation(input_df, s_t, '/Users/har/Desktop/许某/2018质检中心_表头合并后_eval.xls')
