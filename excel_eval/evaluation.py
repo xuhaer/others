94c1e076e81bb8c0f5c11f24b09d652e4550d5f3
hardly <trigal@qq.com>
2018-12-21 14:02:09 +0800

    use json file instead of read_excel

--- excel_eval/evaluation.py
+++ excel_eval/evaluation.py
@@ -7,7 +7,8 @@ import jieba
 import numpy as np
 import pandas as pd
 
-# jieba.load_userdict('st_dict.txt')
+# jieba.load_userdict('./match_dict.txt.txt')
+
 
 def col_type(x):
     """判断某列中某cell的数据类型"""
@@ -33,7 +34,7 @@ def predict_st_names(df, result):
     """
     以json文件为中间件来预测df中各列对应prc库中可能st_name
     """
-    with open('/Users/har/Desktop/st_names_predict.json') as f:
+    with open('./predict_map.json') as f:
         predict_map = json.load(f)
 
     for col in df.columns:
@@ -57,56 +58,53 @@ def col_values_check(df, result):
                 1.df中的带检查列(col)与prc中的st有关联--存在或相似
                 2.col匹配的 matched_name 的dataType 为 int or float
     """
-    s_t = pd.read_excel('/Users/har/Desktop/T_SampleType.xls')
-    s_t.set_index('name', inplace=True)
-    s_t.fillna('', inplace=True)
-    s_t = s_t[s_t['RefRange'] != '']
+    with open('./refRange.json') as f:
+        s_t = json.load(f)
     for col in df.columns:
         if '相似列名' in result[col] or '匹配列名' in result[col]:
             flag = '相似列名' if '相似列名' in result[col] else '匹配列名'
             # 多个匹配项取第一个来预测评估，因此json文件有多匹配的也需按概率大小排序。
             matched_name = result[col][flag].split(',')[0]
-            if not matched_name in s_t.index:
+            if not matched_name in s_t.keys():
                 continue
             # 能到这一步，说明该col类型在prc中为float, 可用 `errors='coerce'` 转换
-            legal_range = json.loads(s_t.loc[matched_name, :]['RefRange'])
-            try:
-                df[col] = pd.to_numeric(df[col], errors='coerce')
-            except ValueError as e:
-                print(f'{e} This should not happen!')
-            if legal_range['max'] == 'Infinity':
-                legal_range['max'] = 9999
-            if df[col].min() < float(legal_range['min']) or \
-                df[col].max() > float(legal_range['max']):
+            legal_range = s_t[matched_name]
+            df[col] = pd.to_numeric(df[col], errors='coerce')
+            if df[col].min() < float(legal_range[0]) or \
+                df[col].max() > float(legal_range[1]):
                 sorted_col = np.sort(df[col].dropna())
-                health_min = bisect_left(sorted_col, legal_range['min'])
-                health_max = len(sorted_col) - bisect_right(sorted_col, legal_range['max'])
+                health_min = bisect_left(sorted_col, legal_range[0])
+                health_max = len(sorted_col) - bisect_right(sorted_col, legal_range[1])
                 result[col].update({'指标异常比例': f'{(health_min + health_max) / len(df[col])*100:.2f}%'})
             else:
                 result[col].update({'指标异常比例': '0%'})
 
 
-def highlight_max_data(data, attr):
-    if data.ndim == 1:
-        is_max = data == data.max()
-        return [attr if v else '' for v in is_max]
-    else:  # from .apply(axis=None)
-        is_max = data == data.max().max()
-        return pd.DataFrame(np.where(is_max, attr, ''),
-                            index=data.index, columns=data.columns)
+def highlight_mixedtype_data(data):
+    """若该列既有str类型，又有numeric 类型则输出为红色"""
+    if data.isnull().values.any():
+        return ['', '']
+    return ['color: red', 'color: red']
+    # if data.ndim == 1:
+    #     is_max = data == data.max()
+    #     return [attr if v else '' for v in is_max]
+    # else:  # from .apply(axis=None)
+    #     is_max = data == data.max().max()
+    #     return pd.DataFrame(np.where(is_max, attr, ''),
+    #                         index=data.index, columns=data.columns)
 
 
 def highlight_unnormal_col(data):
     '该列指标异常值百分比大于30%返回红色cell'
     if isinstance(data, str):
-        return 'color: red' if float(data.replace('%', '')) > 30 else 'color: green'
+        return 'color: red' if float(data.replace('%', '')) >= 30 else 'color: green'
     else:
         return ''
 
 
-def main(input_path, output_path, mul_col):
-    if mul_col != [0]:
-        input_df = pd.read_excel(input_path, header=mul_col)
+def main(input_path, output_path, header=[0]):
+    if header != [0]:
+        input_df = pd.read_excel(input_path, header=header)
         input_df.columns = input_df.columns.map(lambda x: '-'.join(x) if 'Unnamed' not in x[1] else x[0])
         input_df.reset_index(drop=True, inplace=True)
         input_df.drop_duplicates(inplace=True)
@@ -121,15 +119,15 @@ def main(input_path, output_path, mul_col):
     output_df = output_df[['nan', 'numeric', 'str', '匹配列名', '相似列名', '指标异常比例', 'min', 'max', 'unique', 'top', 'freq']]
     try:
         style_df = output_df.style.\
-            applymap(highlight_unnormal_col, subset=['指标异常比例'])
-        #     apply(highlight_illegal_max_min, attr='color: red', axis=1, subset=['合法范围', '非法数据', 'min', 'max'])
+            applymap(highlight_unnormal_col, subset=['指标异常比例']).\
+            apply(highlight_mixedtype_data, axis=1, subset=['numeric', 'str'])
         style_df.to_excel(output_path, engine='openpyxl', float_format='%.3f')
-    except Exception as e:
-        print('染色失败: {e}, 已通过不染色方式输出.')
+    except AttributeError as e:
+        print(f'染色失败: {e}, 已通过不染色方式输出.')
         output_df.to_excel(output_path, float_format='%.3f')
 
 
 if __name__ == '__main__':
     input_path = '/Users/har/Desktop/许某/2018质检中心_表头合并后.xls'
     output_path = '/Users/har/Desktop/许某/2018质检中心_表头合并后_eval.xls'
-    main(input_path, output_path, mul_col=[0])
+    main(input_path, output_path, header=[0])
