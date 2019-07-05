dd6b636dc49ff99ebfbfac8c4d3a9fbd93d7e326
hardly <trigal@qq.com>
2019-07-05 18:24:53 +0800

    fix import error

--- snake_of_china/snake_dis.py
+++ snake_of_china/snake_dis.py
@@ -1,6 +1,7 @@
 from pyecharts.render import make_snapshot
 from pyecharts.charts import Map
-from pyecharts.commons.utils import JsCode
+from pyecharts.commons.utils import JsCodefrom pyecharts import options as opts
+from pyecharts import options as opts
 from snapshot_selenium import snapshot
 
 provs = {'北京': 13, '天津': 10, '重庆': 29, '上海': 12, '河北': 12, '山西': 17, '辽宁': 17, '吉林': 11, '黑龙江': 10, '江苏': 29, '浙江': 60, '安徽': 48, '福建': 85, '江西': 58, '山东': 14, '河南': 26, '湖北': 32, '湖南': 66, '广东': 76, '海南': 66, '四川': 66, '贵州': 74, '云南': 96, '陕西': 33, '甘肃': 36, '青海': 4, '台湾': 58, '内蒙古': 9, '广西': 99, '西藏': 39, '宁夏': 7, '新疆': 10, '香港': 29, '澳门': 15}
