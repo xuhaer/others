8c205650bbf63e9059fb8ef408190acde9c467f3
hardly <trigal@qq.com>
2019-09-26 18:13:23 +0800

    add to_excel in predict

--- excel_eval/predict.py
+++ excel_eval/predict.py
@@ -24,7 +24,7 @@ def name_similarity(origin_name):
         st_name_words = set(jieba.cut(st_name_str)) - {'-', ' ', '_', ')', '('}
         origin_name_words = set(jieba.cut(origin_name)) - {'-', ' ', '_', ')', '('}
         sim = round(len(st_name_words & origin_name_words) / max(len(st_name_words), len(origin_name_words)), 2)
-        origin_name_words_eng = set(re.findall(r'[a-zA-Z]{2,}', origin_name))
+        origin_name_words_eng = set(re.findall(r'[a-zA-Z1-9]{2,}', origin_name))
         st_name_words_eng = set(jieba.cut(st['name'])) - {'-', ' ', '_', ')', '('}
         eng_name_sim = round(len(st_name_words_eng & origin_name_words_eng) / max(len(st_name_words_eng), len(origin_name_words_eng)), 2)
         sim += eng_name_sim
@@ -98,25 +98,40 @@ def cell_type(x):
             return 'numeric'
 
 
-def main(column, col):
+def main(column, col, confirmed_st, unit=None):
+    col_info = {}
+    if column in confirmed_st:
+        col_info['相似度'] = confirmed_st[column]
+        col_info['确认情况'] = '已确认'
+        return col_info
     cnt = Counter(col.map(cell_type))
+    col_info['值的类型'] = dict(cnt)
     most_common_type, *v = cnt.most_common(1)[0]
     if most_common_type == 'numeric':
         test_col = pd.to_numeric(col, errors='coerce')
         test_col = test_col.fillna(method='ffill')
         test_col = test_col.fillna(method='bfill')
-        # col_min, col_max = test_col.min(), test_col.max()
+        col_min, col_max = test_col.min(), test_col.max()
         percent_70 = round(np.percentile(test_col, 15), 2), round(np.percentile(test_col, 85), 2)
-        similarity = predict(origin_name=column, most_common_type=most_common_type, percent_70=percent_70)
+        col_info['最小值'], col_info['最大值'] = round(col_min, 2), round(col_max, 2)
+        col_info['70%的值分布于'] = percent_70
+        similarity = predict(origin_name=column, unit=unit, most_common_type=most_common_type, percent_70=percent_70)
     else:
-        similarity = predict(origin_name=column, most_common_type=most_common_type, most_common_data=dict(Counter(col).most_common(5)).keys())
-    similarity = sorted(similarity, key=itemgetter(0), reverse=True)[:5]
-    return {similarity[i][1]['nameChs']: round(similarity[i][0], 2) for i in range(len(similarity))}
+        most_common_data = dict(Counter(col).most_common(3)).keys()
+        similarity = predict(origin_name=column, unit=unit, most_common_type=most_common_type, most_common_data=most_common_data)
+        col_info['最常见值'] = [f'{data[:4]}...' if len(str(data)) > 6 else str(data) for data in most_common_data]
+    similarity = sorted(similarity, key=itemgetter(0), reverse=True)[:3]
+    col_info['相似度'] = {similarity[i][1]['nameChs']: round(similarity[i][0], 2) for i in range(len(similarity))}
+    col_info['确认情况'] = '无'
+    return col_info
 
 
 if __name__ == '__main__':
+    res = {}
     datas = '/Users/har/Desktop/无锡电信.xlsx'
     df = pd.read_excel(datas).head(10)
     for column in df:
         col = df[column]
-        print(column, main(column, col))
+        res[column] = main(column, col, {'血脂四项:甘油三脂(TG)': '甘油三酯'})
+    df = pd.DataFrame(res)
+    df.T.to_excel('列信息.xlsx')
