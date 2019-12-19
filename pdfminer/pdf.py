f3a37305e67b8b9fa6d3d2ac91faa77f0c12e819
hardly <trigal@qq.com>
2019-12-19 14:43:29 +0800

    extract text from pdf

--- pdfminer/pdf.py
+++ pdfminer/pdf.py
@@ -1,10 +1,8 @@
 """Use pdfminer to extract textbox rows from PDF"""
 import json
 import time
+import re
 import glob
-from pprint import pprint
-
-import arrow
 
 from pdfminer.layout import LAParams, LTExpandableContainer
 from pdfminer.converter import PDFPageAggregator
@@ -58,9 +56,8 @@ def main(fn):
         arr.sort(key=lambda x: -x.y0)
         for line_no, container in enumerate(arr, start=1):
             container._objs.sort(key=lambda x: x.x0)
-            # print(round(container.height, 2), [tb.get_text().replace('\n', '') for tb in container])
-            # print(page_no, line_no, [tb.get_text().replace('\n', '') for tb in container])
             line = [tb.get_text().replace('\n', '') for tb in container]
+            # print(round(container.height, 2), line)
             res.append({'page_no': page_no, 'line_no': line_no, 'data': line})
     return res
 
@@ -81,11 +78,21 @@ def get_cover_info(cover_data):
         '联系电话': cover_data[8]['data'][1] if len(cover_data[8]['data']) == 2 else None,
         '单位': cover_data[9]['data'][1],
         '部门': cover_data[10]['data'][1],
-        # arrow.get(a, 'YYYY年MM月DD日')
-        '体检日期(封面)': cover_data[11]['data'][1],
-        '体检日期(页眉)': f"20{cover_data[1]['data'][0][:6]}",
+        '体检日期': cover_data[11]['data'][1],
     }
     return basic_info
+
+def basic_info_from_content(lines):
+    for line in lines:
+        data = line['data']
+        if data[0] == '姓名：':
+            return {
+                '姓名': data[1],
+                '性别': data[2].replace('性别：', ''),
+                '年龄': re.search(r'\d+', data[3])[0],
+                '流水号': data[5],
+                '体检日期': data[6].replace('体检日期：', ''),
+            }
 
 
 def get_flags(lines):
@@ -96,11 +103,7 @@ def get_flags(lines):
             flags['检查综述起'] = index_
         if data[0].startswith('医生建议'):
             flags['检查综述终'] = index_
-        if data[0].startswith('总检日期'):
-            flags['总检日期'] = index_
-        if '总检日期' in flags and data[0].startswith('姓名') and (index_ - flags['总检日期'] <= 4):
-            flags['第一个检查项'] = index_ + 1
-        if data[0].startswith('操作员'):
+        if data[0].startswith('操作员') or data[0].startswith('总检日期'):
             flags['操作员'].append(index_)
     return flags
 
@@ -113,13 +116,15 @@ def get_summary(lines, flags):
     return summary
 
 
-def get_group_names(slices, flags, i):
-    slices = lines[flags['操作员'][i] + 1:flags['操作员'][i + 1]]
-    if slices[0]['data'][0][:4] not in ['连云港市', '咨询电话', '姓名：']:
-        group_name = slices[0]['data'][0]
-        return group_name
-    else:
-        raise ValueError('group_name 不合法！') from None
+def get_group_names(slices):
+    for sl in slices:
+        if sl['data'][0][:4] not in ['连云港市', '咨询电话', '姓名：']:
+            group_name = sl['data'][0]
+            return group_name
+        else:
+            continue
+    raise ValueError('group_name 不合法！') from None
+
 
 def get_group_samples(slices):
     group_samples = []
@@ -129,36 +134,118 @@ def get_group_samples(slices):
     return group_samples
 
 
-def get_samples(lines):
-    flags = get_flags(lines)
-    res = {}
-    summary = get_summary(lines, flags)
-    res['综述'] = summary
+def parsing_common_samples(group_samples, group_name):
+    res = []
+    for line in group_samples:
+        if len(line) == 1:
+            continue
+        elif len(line) == 2:
+            if line[0].startswith('小结：'):
+                res.append({'group_name': group_name, 'item_name': f'{group_name}小结', 'value': line[1]})
+            elif line[0] != '项目名称':
+                res.append({'group_name': group_name, 'item_name': line[0], 'value': line[1]})
+            else:
+                continue
+        elif len(line) in [3, 4, 5]:
+            if '项目名称' in str(line) or '单位' in str(line): continue
+            if ['项目名称', '检查结果', '单位', '参考值', '提示'] in group_samples:
+                refrange = None
+                # ['RBC分布宽度(CV)', '12.6', '11-16'] 无单位的情况，当然后面可有提示值
+                # ['维生素C', '-', 'nmol/L']           无参考值的情况
+                for data in line:
+                    if re.search(r'\d+\.?\d*-+\d+\.?\d*', str(data)) or (set('<=>') & set(data)):
+                        refrange = data
+                if refrange:
+                    res.append({'group_name': group_name, 'item_name': line[0], 'value': line[1], 'refrange': refrange})
+                else:
+                    res.append({'group_name': group_name, 'item_name': line[0], 'value': line[1]})
+            else:
+                raise ValueError(f'体检数据未知的长度格式:{line}') from None
+        else:
+            raise ValueError(f'体检数据未知的长度格式:{line}') from None
+    return res
 
+
+def parsing_divide_samples(group_samples, group_name):
+    res = []
+    for line in group_samples:
+        if len(line) == 1:
+            continue
+        elif len(line) == 2:
+            if line[0].startswith('小结：'):
+                res.append({'group_name': group_name, 'item_name': f'{group_name}小结', 'value': line[1]})
+            elif line[0].startswith('其') or line[0].startswith('液基细胞'):# 其它 和 其他
+                res.append({'group_name': group_name, 'item_name': line[0], 'value': line[1]})
+            elif '视力' in line[0]:
+                for l in line:
+                    if len(l.split()) == 2:
+                        res.append({'group_name': group_name, 'item_name': l.split()[0], 'value': l.split()[1]})
+                    else:
+                        continue
+            else:
+                continue
+        elif len(line) == 3:
+            # ['宫颈', '宫颈肥大  宫颈糜烂Ⅱ度', '子宫体位置 正常']
+            if line == ['身高', 'Cm', '体重']:
+                continue
+            if len(line[2].split()) != 2:
+                continue
+            res.append({'group_name': group_name, 'item_name': line[0], 'value': line[1]})
+            res.append({'group_name': group_name, 'item_name': line[2].split()[0], 'value': line[2].split()[1]})
+        elif len(line) == 4:
+            # ['家族史', '无', '既往史', '无']
+            res.append({'group_name': group_name, 'item_name': line[0], 'value': line[1]})
+            res.append({'group_name': group_name, 'item_name': line[2], 'value': line[3]})
+        elif len(line) == 5:
+            # ['身高', '162.3', 'Cm', '体重', '56.0']
+            res.append({'group_name': group_name, 'item_name': line[0], 'value': line[1]})
+            res.append({'group_name': group_name, 'item_name': line[3], 'value': line[4]})
+        else:
+            raise ValueError(f'体检数据未知的长度格式:{line}') from None
+    return res
+
+
+def parsing_samples(group_name, group_samples):
+    if '项目名称' in str(group_samples):
+        res = parsing_common_samples(group_samples, group_name)
+    else:
+        res = parsing_divide_samples(group_samples, group_name)
+    return res
+
+
+def get_data(lines):
+    flags = get_flags(lines)
+    summary = get_summary(lines, flags)
+    all_samples = []
     for i in range(len(flags['操作员']) - 1):
         slices = lines[flags['操作员'][i] + 1:flags['操作员'][i + 1]]
-        group_name = get_group_names(slices, flags, i)
-        print(f'{group_name: ^30}')
+        group_name = get_group_names(slices)
         group_samples = get_group_samples(slices)
-        pprint(group_samples)
-        break
-
-    # if index_ > flags['第一个检查项'] and line['data'][0].startswith('操作员'):
-    #     group_sep_index = index_
+        samples = parsing_samples(group_name, group_samples)
+        all_samples.extend(samples)
+    return {'summary': summary, 'samples': all_samples}
 
 
 if __name__ == '__main__':
     a = time.time()
-    paths = glob.glob('/Users/har/Desktop/连云港第一人民医院526/*.pdf')
-    fails = 0
+    res = []
+    paths = glob.glob('/Users/har/Desktop/连云港第一人民医院534/*.pdf')
     for path in paths:
-        # path = '/Users/har/Desktop/连云港第一人民医院526/o2UrD0gdNAuigatnFnTnhqocmtZE.pdf'
         lines = main(path)
+        if not lines:
+            # pdf 不含文字内容，全为图片
+            print(path)
+            res.append({'file_name': path})
+            continue
         cover_data = [line for line in lines if line['page_no'] == 1]
-        get_samples(lines)
-        print(path)
-        break
-        # if not cover_data:
-        #     pass
-        # else:
-        #     basic_info = get_cover_info(cover_data, path)
+        if not cover_data:
+            basic_info = basic_info_from_content(lines)
+        else:
+            basic_info = get_cover_info(cover_data)
+        data = {'file_name': path, 'basic_info': basic_info}
+        data.update(get_data(lines))
+        res.append(data)
+        print('ok', end=' ')
+    with open('/Users/har/Desktop/data2.json', 'w') as f:
+        json.dump(res, f, ensure_ascii=False, indent=2)
+    print(time.time() - a)
