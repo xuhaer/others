395aed05233c7ef22007bc55bc276818c3aaae66
hardly <trigal@qq.com>
2020-02-29 17:17:05 +0800

    improve word_table.py

--- wordminer/word_table.py
+++ wordminer/word_table.py
@@ -1,9 +1,14 @@
 '''
     将.docx文档中的表格数据提取成Pandas的DataFrame
-    缺陷是没法判断紧邻表格前的表格说明信息
 '''
 import pandas as pd
+
 from docx.api import Document
+from docx.oxml.text.paragraph import CT_P
+from docx.oxml.table import CT_Tbl
+from docx.table import Table
+from docx.text.paragraph import Paragraph
+
 
 def table_to_df(table):
     '''用于中行体检报告数据（泰州第二人民医院）'''
@@ -20,8 +25,32 @@ def table_to_df(table):
     df = pd.DataFrame(data)
     return df
 
+
+def iter_block_items(document):
+    document_elm = document.element.body
+    for child in document_elm.iterchildren():
+        if isinstance(child, CT_P):
+            yield Paragraph(child, document)
+        elif isinstance(child, CT_Tbl):
+            yield Table(child, document)
+
+
+def print_extract_document_data(document):
+    '''从Document中提取表格数据并保留其表头信息'''
+    global table_header
+
+    for block in iter_block_items(document):
+        # 表头信息保留，表尾信息舍弃，表格数据的上一个值为有效的表头信息
+        if isinstance(block, Paragraph):
+            t_header = block.text
+            if t_header != '\n':
+                table_header = t_header
+        elif isinstance(block, Table):
+            table_df = table_to_df(block)
+            if not table_df.empty:
+                print(table_header, ':')
+                print(table_df)
+                print()
+
 document = Document('/Users/har/Desktop/19093517_徐文娟.docx')
-for table in document.tables:
-    df = table_to_df(table)
-    print(df)
-    print()
+print_extract_document_data(document)
