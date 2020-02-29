d1bb8c9d9dc6faf95b0f06e717f0ffc1cf6835be
hardly <trigal@qq.com>
2020-02-29 09:45:36 +0800

    宿迁鼓楼医院160份

--- pdfminer/pdf.py
+++ pdfminer/pdf.py
@@ -1,7 +1,8 @@
 """Use pdfminer to extract textbox rows from PDF"""
+import os
+import re
 import json
 import time
-import re
 import glob
 
 from pdfminer.layout import LAParams, LTExpandableContainer
@@ -41,7 +42,7 @@ def bsearch(arr, tb):
 def main(fn):
     """将pdfminer解析得到的textboxes按y1值进行分组"""
     res = []
-    laparams = LAParams(line_margin=0.2)
+    laparams = LAParams(line_margin=0.32)
     pages = get_pages(fn, laparams)
     for page_no, textboxes in enumerate(pages, start=1):
         arr = []
@@ -64,23 +65,30 @@ def main(fn):
 
 
 def get_cover_info(cover_data):
-    assert cover_data[0]['data'][0].endswith('体检报告')
-    assert cover_data[1]['data'][0].startswith('体检号: ')
-    assert cover_data[2]['data'][0].startswith('姓  名: ')
-    assert cover_data[3]['data'][0].startswith('单  位: ')
-    assert cover_data[4]['data'][0].startswith('部  门: ')
-    assert cover_data[5]['data'][0].startswith('打印日期：2019年')
-
-    checkup_num = cover_data[1]['data'][0].split()[1]
-    assert checkup_num.startswith('19') and len(checkup_num) == 11
+    assert cover_data[0]['data'][0].startswith('南京鼓楼')
+    assert cover_data[1]['data'][0].startswith('健康')
+    assert cover_data[2]['data'][0].startswith('档 案 号')
+    assert cover_data[3]['data'][0].startswith('姓    名')
+    assert cover_data[4]['data'][0].startswith('单位名称')
+    assert cover_data[5]['data'][0].startswith('总检日期')
+    assert cover_data[8]['data'][0].startswith('部    门')
+    assert cover_data[9]['data'][0].startswith('手    机')
+    # assert cover_data[12]['data'][1].startswith('体检号')
+    for data in cover_data:
+        if '龄' in str(data) and '性' in str(data):
+            sex_age = data['data']
+        if '体检号' in str(data):
+            check_num = data['data']
     basic_info = {
-        '体检号': checkup_num,
-        '姓名': cover_data[2]['data'][0][6:],
-        '性别': cover_data[2]['data'][1].split()[3],
-        '年龄': cover_data[2]['data'][1].split()[1],
-        '单位': cover_data[3]['data'][0][6:],
-        '部门': cover_data[4]['data'][0][6:],
-        '打印日期': cover_data[5]['data'][0][5:],
+        '档案号': cover_data[2]['data'][1].strip(),
+        '姓名': cover_data[3]['data'][1].strip(),
+        '单位名称': cover_data[4]['data'][1].strip(),
+        '总检日期': cover_data[5]['data'][1].strip(),
+        '性别': sex_age[1].split('：')[1].strip(),
+        '年龄': sex_age[2].split('：')[1].replace('岁', '').strip(),
+        '部门': cover_data[8]['data'][1].strip() if len(cover_data[8]['data']) == 2 else None,
+        '手机': cover_data[9]['data'][1].strip() if len(cover_data[9]['data']) == 2 else None,
+        '体检号': re.search(r'体检号：(\d)*', ''.join(check_num))[0].split('：')[1].strip(),
     }
     return basic_info
 
@@ -102,16 +110,15 @@ def get_flags(lines):
     flags = {'项目指标锚点': []}
     for index_, line in enumerate(lines):
         data = line['data']
-        if data[0].startswith('一、各科检查汇总'):
+        if data[0].startswith('本次体检中存在的异常提示'):
             flags['检查综述起'] = index_
-        if data[0].startswith('二、体检结论及医生建议'):
+        if data[0].startswith('总检建议'):
             flags['检查综述终'] = index_
-        if ('医师' in str(data) and lines[index_ - 1]['data'][0] != '检查结论') \
-        or '审核日期' in str(data) \
-        or (data[0].startswith('【') and data[0] != '【各科检查结果记录】'):
+        if '检查医生' in str(data):
             flags['项目指标锚点'].append(index_)
-    if flags.keys() != {'检查综述起', '检查综述终', '项目指标锚点'}:
-        raise ValueError('标记字段不全！')
+    flags['项目指标锚点'].append(len(lines)) # 强制标记结尾，有冗余
+    # if flags.keys() != {'项目指标锚点', '检查综述起', '检查综述终'}:
+    #     raise ValueError('信息不全')
     return flags
 
 
@@ -119,103 +126,64 @@ def get_summary(lines, flags):
     summary = ''
     for index_, line in enumerate(lines):
         if flags['检查综述起'] <= index_ < flags['检查综述终']:
-            summary += f"\n{line['data'][0]}"
+            summary += f"\n{'  :'.join(line['data'])}"
     return summary
 
 
 def get_group_names(slices):
     for sl in slices:
-        if sl['data'][0][:4] not in ['美好人生', '说明：如', '审核时间']:
-            # 跨页时有页眉页脚多余数据
-            group_name = sl['data'][0]
-            return group_name
+        # 跨页时有页眉页脚多余数据:
+        for other_info in ['南京鼓楼', '地址', '---姓', '姓名']:
+            if not other_info in sl['data'][0]:
+                group_name = sl['data'][0]
+                return group_name
 
 
 def get_group_samples(slices):
     group_samples = []
     for samples in slices[1:]:
-        if samples['data'][0][:4] not in ['美好人生', '说明：如']:
+        if not (samples['data'][0].startswith('项目名称') or \
+        samples['data'][0].startswith('南京') or \
+        samples['data'][0].startswith('检查结果') or \
+        samples['data'][0].startswith('地址') or \
+        samples['data'][0].startswith('姓名') or \
+        samples['data'][0] == ' ' or \
+        samples['data'][0].startswith('---姓')):
             group_samples.append(samples['data'])
     return group_samples
 
 
 def parsing_common_samples(group_samples, group_name):
     res = []
-    if group_samples[0] == ['检查描述']:
-        # 彩超综述段落
-        group_samples.remove(['检查描述'])
-        group_samples.remove(['检查结论'])
-        for index_, text in enumerate(group_samples):
-            if '审核时间：' in str(text) or '医师' in str(text):
-                group_samples.pop(index_)
-        if isinstance(group_samples[0], list):
-            # fix: group_samples内部的值也有可能均为列表，而且可能有多个值共同为描述字段
-            group_samples = ['\n'.join(x) for x in group_samples]
-        res.append({'group_name': group_name, 'item_name': f'{group_name}描述', 'value': '\n'.join(group_samples[:-1])})
-        res.append({'group_name': group_name, 'item_name': f'{group_name}结论', 'value': group_samples[-1]})
-        return res
     for line in group_samples:
-        if len(line) == 1:
+        if '项目名称' in str(line):
             continue
-        elif len(line) == 2:
-            if line[0].startswith('小结：'):
-                pass
-                # res.append({'group_name': group_name, 'item_name': f'{group_name}小结', 'value': line[1]})
-            elif line[0] != '项目名称':
-                res.append({'group_name': group_name, 'item_name': line[0], 'value': line[1]})
+        if len(line) == 1:
+            if '小结' in str(line):
+                res.append({'group_name': group_name, 'item_name': f'{group_name.strip()}小结', 'value': line[0].strip()})
+            elif not (line[0].startswith('辅诊') or \
+                line[0].startswith('实验室') or \
+                line[0].startswith('(FPSA)') or \
+                line[0].startswith('心电图检查报告') or \
+                line[0].startswith('辅诊')):
+                res[-1]['value'] += line[0].strip()
             else:
-                continue
+                print(f'请注意line{line} 是否为无效数据??')
+                # raise ValueError('未知数据格式！{line}') from None
+        elif len(line) == 2:
+            res.append({'group_name': group_name, 'item_name': line[0].strip(), 'value': line[1].strip()})
         elif len(line) in [3, 4, 5]:
-            if '项目名称' in str(line) or '单位' in str(line):
+            if len(line) == 3 and line[2].strip() == '异常':# 彩超提示
+                res.append({'group_name': group_name, 'item_name': line[0].strip(), 'value': line[1].strip()})
                 continue
             refrange = None
             for data in line:
                 if re.search(r'\d+\.?\d*-+\d+\.?\d*', str(data)) or (set('<=>') & set(data)):
                     refrange = data
             if refrange:
-                res.append({'group_name': group_name, 'item_name': line[0], 'value': line[1], 'refrange': refrange})
+                res.append({'group_name': group_name, 'item_name': line[0].strip(), 'value': line[1].strip(), 'refrange': refrange})
             else:
-                res.append({'group_name': group_name, 'item_name': line[0], 'value': line[1]})
-        else:
-            raise ValueError(f'体检数据未知的长度格式:{line}') from None
-    return res
-
-
-def parsing_divide_samples(group_samples, group_name):
-    '''处理一行有2个指标检查项的情况'''
-    res = []
-    for line in group_samples:
-        if len(line) == 1:
-            continue
-        elif len(line) == 2:
-            if line[0].startswith('小结：'):
-                res.append({'group_name': group_name, 'item_name': f'{group_name}小结', 'value': line[1]})
-            elif line[0].startswith('其') or line[0].startswith('液基细胞'):# 其它 和 其他
-                res.append({'group_name': group_name, 'item_name': line[0], 'value': line[1]})
-            elif '视力' in line[0]:
-                for l in line:
-                    if len(l.split()) == 2:
-                        res.append({'group_name': group_name, 'item_name': l.split()[0], 'value': l.split()[1]})
-                    else:
-                        continue
-            else:
-                continue
-        elif len(line) == 3:
-            # ['宫颈', '宫颈肥大  宫颈糜烂Ⅱ度', '子宫体位置 正常']
-            if line == ['身高', 'Cm', '体重']:
-                continue
-            if len(line[2].split()) != 2:
-                continue
-            res.append({'group_name': group_name, 'item_name': line[0], 'value': line[1]})
-            res.append({'group_name': group_name, 'item_name': line[2].split()[0], 'value': line[2].split()[1]})
-        elif len(line) == 4:
-            # ['家族史', '无', '既往史', '无']
-            res.append({'group_name': group_name, 'item_name': line[0], 'value': line[1]})
-            res.append({'group_name': group_name, 'item_name': line[2], 'value': line[3]})
-        elif len(line) == 5:
-            # ['身高', '162.3', 'Cm', '体重', '56.0']
-            res.append({'group_name': group_name, 'item_name': line[0], 'value': line[1]})
-            res.append({'group_name': group_name, 'item_name': line[3], 'value': line[4]})
+                res.append({'group_name': group_name, 'item_name': line[0].strip(), 'value': line[1].strip()})
         else:
             raise ValueError(f'体检数据未知的长度格式:{line}') from None
     return res
@@ -232,19 +200,24 @@ def parsing_samples(group_name, group_samples):
 
 def get_data(lines):
     flags = get_flags(lines)
-    summary = get_summary(lines, flags)
+    try:
+        summary = get_summary(lines, flags)
+    except Exception:
+        summary = ''
     all_samples = []
     for i in range(len(flags['项目指标锚点']) - 1):
         slices = lines[flags['项目指标锚点'][i]:flags['项目指标锚点'][i + 1]]
         if not slices:
             continue
-        group_name = get_group_names(slices)
+        # group_name = get_group_names(slices)
+        group_name = lines[flags['项目指标锚点'][i]]['data'][0].strip()
+        group_name = group_name.replace('项目名称', '').strip()
         if not group_name:
-            # print(f'请注意{slices} 是否为无效数据??')
+            print(f'请注意group_name{slices} 是否为无效数据??')
             continue
         group_samples = get_group_samples(slices)
         if not group_samples:
-            # print(f'请注意{slices} 是否为无效数据??')
+            print(f'请注意group_samples{slices} 是否为无效数据??')
             continue
         samples = parsing_samples(group_name, group_samples)
         all_samples.extend(samples)
@@ -254,22 +227,24 @@ def get_data(lines):
 if __name__ == '__main__':
     a = time.time()
     res = []
-    paths = glob.glob('/Users/har/Desktop/盐城市中医院/*.pdf')
-    # paths = glob.glob('/Users/har/Desktop/1/*.pdf')
+    paths = glob.glob('/Users/har/Desktop/pdf/*.pdf')
     for path in paths:
+        # path = '/Users/har/Desktop/pdf/o2UrD0hMwrwTzqEX_0saTeJ-9M14.pdf'
         lines = main(path)
-        # path = '/Users/har/Desktop/盐城市中医院/o2UrD0tLur0lyp6HUDBY0i0HTd3s.pdf'
         if not lines:
             # pdf 不含文字内容，全为图片
             res.append({'file_name': path})
             continue
-        cover_data = [line for line in lines if line['page_no'] == 1]
+        cover_data = [line for line in lines if line['page_no'] <= 2]
         if not cover_data:
             raise ValueError(f'该份报告没有封面信息！----{path}')
             # basic_info = basic_info_from_content(lines)
         else:
-            basic_info = get_cover_info(cover_data)
-        data = {'file_name': path, 'basic_info': basic_info}
+            try:
+                basic_info = get_cover_info(cover_data)
+            except Exception as e:
+                print(path)
+        data = {'file_name': os.path.basename(path), 'basic_info': basic_info}
         data.update(get_data(lines))
         res.append(data)
     with open('/Users/har/Desktop/pdf1.json', 'w') as f:
