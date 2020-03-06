92f9192a970f74a0754acf2eb4484f7c41593151
hardly <trigal@qq.com>
2020-03-06 16:31:47 +0800

    update simplify_json.py

--- simplify_json.py
+++ simplify_json.py
@@ -1,10 +1,12 @@
+import os
 import json
+import glob
 
 
-def simplify_json(file_name):
+def simplify_json(origin_path):
+    '''简化生成好的配置规则'''
     res = []
-    path = '/Users/har/Documents/github/others/'
-    with open(f'{path}{file_name}.json') as f:
+    with open(origin_path) as f:
         origin_data = json.load(f)
 
     for st in origin_data:
@@ -26,33 +28,37 @@ def simplify_json(file_name):
                 'std_name': {'name': st['std_name']['name']},
             })
 
-    with open(f'{path}{file_name}_精简版.json', 'w') as f:
+    with open('_简化版'.join(os.path.splitext(path)), 'w') as f:
         json.dump(res, f, ensure_ascii=False, indent=2)
 
 
-simplify_json('滨海县人民医院')
-
-
-def lazy_json():
-    with open('/Users/har/Documents/github/others/配置规则/爱康国宾盐城.json') as f:
+def auto_correct_json_rule(origin_path, dest_path):
+    ''' 为了省时省力，和已经配置好的配置规则做group_name和item_name的全匹配，
+        若直接匹配，则将配置好的规则赋值给生成的新规则
+    '''
+    with open(origin_path) as f:
         origin_data = json.load(f)
 
-    with open('/Users/har/Documents/github/others/配置规则/爱康国宾连云港.json') as f:
-        ref_data = json.load(f)
+    for path in glob.glob('/Users/har/Documents/github/others/参考配置规则/*.json'):
+        with open(path) as f:
+            ref_data = json.load(f)
 
-    for origin_st in origin_data:
-        for ref_st in ref_data:
-            if origin_st['group_name'] == ref_st['group_name'] and \
-                origin_st['item_name'] == ref_st['item_name']:
-                # if origin_st['std_name'] != ref_st['std_name']:
-                #     print(origin_st['item_name'])
-                origin_st['std_name'] = ref_st['std_name']
-                origin_st['function'] = ref_st['function']
-                # print(origin_st['item_name'])
-                break
-        print(origin_st['item_name'])
+        for origin_st in origin_data:
+            for ref_st in ref_data:
+                if origin_st['group_name'] == ref_st['group_name'] and \
+                    origin_st['item_name'] == ref_st['item_name']:
+                    # if origin_st['std_name'] != ref_st['std_name']:
+                    #     print(origin_st['item_name'])
+                    origin_st['std_name']['name'] = ref_st['std_name']['name']
+                    origin_st['function'] = ref_st['function']
+                    break
 
-    with open('/Users/har/Documents/github/others/爱康国宾盐城_lazy.json', 'w') as f:
+    with open(dest_path, 'w') as f:
         json.dump(origin_data, f, ensure_ascii=False, indent=2)
 
-# lazy_json()
+
+if __name__ == '__main__':
+    for path in glob.glob('/Users/har/Desktop/预测配置结果/*.json'):
+        dest_path = '_lazy'.join(os.path.splitext(path))
+        auto_correct_json_rule(path, dest_path)
+        # simplify_json(dest_path)
